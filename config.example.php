0861f788eff384d4b53e74959839396f9fab0588
mattab <matthieu.aubry@gmail.com>
2016-04-19 12:06:53 +1200

    Cleanup output + add config setting to enable attachment download

--- config.example.php
+++ config.example.php
@@ -16,6 +16,12 @@ $backup_closed_boards = false;
 // Backup all Trello Boards from the organizations that the user has read access to
 $backup_all_organization_boards = false;
 
+// Backup all cards' attachments in a subfolder for each Trello board
+$backup_attachments = false;
+
+// Where to store the files (by default, trello boards JSON files will be stored in this directory
+$path = dirname(__FILE__);
+
 // HTTP proxy, if one is required, in the format 'host:port', e.g. 'proxy.example.com:80' or '192.168.1.254:8080'
 $proxy= '';
 
